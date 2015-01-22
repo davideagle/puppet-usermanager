648e3c74ce0289e37fc4e1fc0dc1c886abbe0c8a
Davíð Örn Jóhannsson <davidoj@siminn.is>
2015-01-22 11:54:18 +0000

    fixed comments and varables

--- manifests/group.pp
+++ manifests/group.pp
@@ -1,3 +1,13 @@
+# Definition: usermanager::user
+#
+# Manage deployment of users and groups.
+#
+# Parameters:
+# - $ensure
+# - $gid is the groupid
+# - $ensure
+# - $system
+#
 define usermanager::group(
   $ensure = present,
   $gid    = undef,
@@ -5,7 +15,7 @@ define usermanager::group(
   
 
 ){
-  ::group{ $gid:
+  ::group{ $title:
     ensure => $ensure,
     gid    => $gid,
     system => $system,
--- manifests/user.pp
+++ manifests/user.pp
@@ -3,22 +3,16 @@
 # Manage deployment of users and groups.
 #
 # Parameters:
-# - $catalina_base is the base directory for the Tomcat installation
-# - $app_base is the path relative to $catalina_base to deploy the WAR to.
-#   Defaults to 'webapps'.
-# - The $deployment_path can optionally be specified. Only one of $app_base and
-#   $deployment_path can be specified.
-# - $war_ensure specifies whether you are trying to add or remove the WAR.
-#   Valid values are 'present', 'absent', 'true', and 'false'. Defaults to
-#   'present'.
-# _ Optionally specify a $war_name. Defaults to $name.
-# - $war_purge is a boolean specifying whether or not to purge the exploded WAR
-#   directory. Defaults to true. Only applicable when $war_ensure is 'absent'
-#   or 'false'. Note: if tomcat is running and autodeploy is on, setting
-#   $war_purge to false won't stop tomcat from auto-undeploying exploded WARs.
-# - $war_source is the source to deploy the WAR from. Currently supports
-#   http(s)://, puppet://, and ftp:// paths. $war_source must be specified
-#   unless $war_ensure is set to 'false' or 'absent'.
+# - $uid is the userid
+# - $ensure 
+# - $shell
+# - $groups
+# - $password
+# - $managehome
+# - $home
+# - $comment
+#
+
 define usermanager::user(
   $uid        = undef,  
   $ensure     = 'present', 
@@ -36,7 +30,7 @@ define usermanager::user(
     $_home = "/home/${uid}"
   }
   
-  ::user{ $uid:
+  ::user{ $title:
     ensure => $ensure,
     uid    => $uid,
     gid    => $uid,
@@ -49,7 +43,7 @@ define usermanager::user(
   }
 
   
-  usermanager::group { $uid:
+  usermanager::group { $title:
     ensure => $ensure,
     gid    => $uid,
   }
